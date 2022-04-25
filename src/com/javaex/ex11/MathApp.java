8ad573b669d5c8cf9f8c685fa9e687c57308676c
가은 <rkdms8228@naver.com>
2022-04-25 15:01:37 +0900

    math 스태틱

---
+++ src/com/javaex/ex11/MathApp.java
@@ -0,0 +1,20 @@
+package com.javaex.ex11;
+
+public class MathApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.plus(3, 2));
+		System.out.println(Math.plus(3.5, 2.2));
+		//스태틱에 있는 값을 사용함
+		
+		/*
+		Math math = new Math();
+		System.out.println(math.circleArea(5));
+		*/
+		
+		System.out.println(Math.circleArea(5));
+		
+	}
+
+}
